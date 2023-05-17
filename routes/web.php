703c984fcf537bc6e7b7e6a324da513bd1140e81
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-05-17 16:29:48 -0700

    add: the other views for the login/Auth functionality

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,13 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::controller(UserController::class)->group(function () {    
+Route::controller(UserController::class)->group(function () {
     // Route::get('/users', [UserController::class, 'index'])->name('Users.users');
-    Route::get('/users', 'index')->name('Users.users');    
-    // Route::get('/edit/{id}', [UserController::class, 'edit'])->name('Users.edit');
-    Route::get('/edit/{id}', 'edit')->name('Users.edit');    
-    // Route::post('/user/update_ok/{id}', [UserController::class, 'updated'])->name('Users.update_ok');
-    Route::post('/user/update_ok/{id}', 'updated')->name('Users.update_ok');    
-    // Route::get('/user/create', [UserController::class, 'create'])->name('Users.create');
-    Route::get('/user/create', 'create')->name('Users.create');    
-    // Route::post('/user/insert', [UserController::class, 'insert'])->name('Users.insert_ok');
-    Route::post('/user/insert', 'insert')->name('Users.insert_ok');    
-    // Route::get('/user/remove/{id}', [UserController::class, 'destroy'])->name('Users.deleted_ok');
+    Route::get('/users', 'index')->name('Users.users');
+    Route::get('/edit/{id}', 'edit')->name('Users.edit');
+    Route::post('/user/update_ok/{id}', 'updated')->name('Users.update_ok');
+    Route::get('/user/create', 'create')->name('Users.create');
+    Route::post('/user/insert', 'insert')->name('Users.insert_ok');
     Route::get('/user/remove/{id}', 'destroy')->name('Users.deleted_ok');
 });
 
