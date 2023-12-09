9772b1ebfac811d6dbc06b55f01f4cb5c371167c
Avelazquez <avelazquez@abogadoericprice.com>
2023-12-08 17:11:29 -0800

    UPDATE: the unit test, now we have a unit test only to chek if all works, and now we can create all of unit test for all project

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    // public function test_example(): void
+    // {
+    //     $this->assertTrue(true);
+    // }
+
+    public function test_basic_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
